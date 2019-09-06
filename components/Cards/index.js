d79324874af83b0107b837729b920232e4f7efe6
DimejiAre <olardi2@gmail.com>
2019-09-06 09:20:18 +0100

    create Card component and add cards to dom

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function Card(data){
+    const [cardDiv, headDiv, authorDiv, imgDiv, img, span,] = ['div', 'div', 'div','div','img','span']
+        .map(element => document.createElement(element));
+    
+    cardDiv.classList.add('card');
+    headDiv.classList.add('headliine');
+    authorDiv.classList.add('author');
+    imgDiv.classList.add('img-container');
+
+    headDiv.textContent = data.headline
+    img.src = data.authorPhoto;
+    span.textContent = `By ${data.authorName}`;
+
+    imgDiv.appendChild(img);
+
+    authorDiv.appendChild(imgDiv);
+    authorDiv.appendChild(span);
+
+    cardDiv.appendChild(headDiv);
+    cardDiv.appendChild(authorDiv);
+
+    return cardDiv;
+}
+
+function addCards(){
+    const htmlCards = document.querySelector('.cards-container');
+
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+        .then(response => {
+            let data = response.data.articles;
+            for (let key in data){
+                data[key].map(element => {
+                    htmlCards.appendChild(Card(element));
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+addCards()
